fdea89ce2049cebee2fd44b07969c0113a31a190
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-20 01:44:39 -0500

    Clean code hack

--- Conditionals/conditionals.go
+++ Conditionals/conditionals.go
@@ -6,5 +6,14 @@ func main() {
 	height := 5
 	if height > 4 {
 		fmt.Println("You are tall enough")
+	} else {
+		fmt.Println("You are not tall enough")
+	}
+
+	//Alternate
+	if length := 5; length > 4 {
+		fmt.Printf("You may pass")
+	} else {
+		fmt.Println("Stop")
 	}
 }
