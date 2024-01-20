4f39408823e5ebe07b1208a0feced2cde9e40792
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-20 02:33:45 -0500

    Structs

---
+++ Structs/structs.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Create an instance of the struct
+	myCar := car{}
+	myCar.FrontWheel.Radius = 5
+
+	fmt.Println(myCar.FrontWheel.Radius)
+}
+
+type car struct {
+	Make       string
+	Model      string
+	Height     int
+	Width      int
+	FrontWheel Wheel
+	BackWheel  Wheel
+}
+
+type Wheel struct {
+	Radius   int
+	Material string
+}
