71bbb81ce7faec42b13ce65bd47f85c87bb2ec2d
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-20 02:11:13 -0500

    functions, updated

--- Functions/functions.go
+++ Functions/functions.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println(add1(5, 6))
 	fmt.Println(add2(5, 6))
+
+	a, b := addAndSub(5, 6)
+	fmt.Println(a, b)
+
+	//To ignore one value out of many return values
+	c, _ := addAndSub(5, 6)
+	fmt.Println(c)
 }
 
 func add1(x int, y int) int {
@@ -14,4 +21,9 @@ func add1(x int, y int) int {
 // Alternate
 func add2(x, y int) int {
 	return x + y
+}
+
+// Functions in go can return multiple values
+func addAndSub(x, y int) (int, int) {
+	return x + y, x - y
 }
