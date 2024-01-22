f1b255f39c86d00ca3b24f81f614be8f83821775
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-22 06:34:28 -0500

    Slices Functions

--- Arrays/arrays.go
+++ Arrays/arrays.go
@@ -11,6 +11,15 @@ func main() {
 	//Slices
 	b := integers[0:2]
 
+	//Make to create new slices
+	mySlice := make([]int, 5, 10)
+
 	fmt.Println(integers)
 	fmt.Println(b)
+	fmt.Println(len(mySlice))
+
+	//Length func for slices
+	fmt.Println(len(mySlice))
+	//Capacity func for slices
+	fmt.Println(cap(mySlice))
 }
