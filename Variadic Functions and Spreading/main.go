ff0d6d633439059c433b6ba1ad27173bf388d231
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-22 06:58:39 -0500

    Variadic Functions

---
+++ Variadic Functions and Spreading/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Calling Variadic function
+	fmt.Println(sum(1, 2, 3, 4))
+}
+
+// Variadic Function:
+func sum(nums ...int) int { // Where ...int is a slice
+	var num int = 0
+	for i := 0; i < len(nums); i++ {
+		num += nums[i]
+	}
+	return num
+}
