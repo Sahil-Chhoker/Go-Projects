4ac397932c23b35e18de3f34be228b6b1bcad1c7
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-22 04:33:46 -0500

    While loop and for loop omitting

--- Loops/loops.go
+++ Loops/loops.go
@@ -6,6 +6,26 @@ import (
 
 func main() {
 	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+		fmt.Print(i)
 	}
+
+	//Example of while loop
+	plantHeight := 1
+	for plantHeight < 5 {
+		fmt.Println("Still growing!! Current Height : ", plantHeight)
+		plantHeight++
+	}
+
+	fmt.Println("Plant has grown to ", plantHeight, " inches")
 }
+
+//We can ommit for loops in GO
+// for Initial ; ; After{
+
+// }
+//The above code is totally valid
+
+//Therfore, the while loop in GO is:
+// for CONDITION{
+
+// }
