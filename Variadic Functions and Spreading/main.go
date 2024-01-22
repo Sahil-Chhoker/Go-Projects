6ecdedf3ee98e294b114734dca94ec64183cc243
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-22 07:16:19 -0500

    Spread Operators

--- Variadic Functions and Spreading/main.go
+++ Variadic Functions and Spreading/main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 	//Calling Variadic function
 	fmt.Println(sum(1, 2, 3, 4))
+
+	//Calling by Spread Operators:
+	names := []string{"bob", "sue", "damn"}
+	printStrings(names...)
 }
 
 // Variadic Function:
@@ -16,4 +20,13 @@ func sum(nums ...int) int { // Where ...int is a slice
 		num += nums[i]
 	}
 	return num
+}
+
+//Spread Operators:
+
+// Variadic func
+func printStrings(strings ...string) {
+	for i := 0; i < len(strings); i++ {
+		fmt.Println(strings[i])
+	}
 }
