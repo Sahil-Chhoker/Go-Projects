f08fa3b6b88da1c16daf30b8b222b901a2d637ee
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-23 06:58:17 -0500

    Creating a map and adding values to it

--- Files/maps.go
+++ Files/maps.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+//They are dictionaries in python, a key is paired with a value
+
 func main() {
-	fmt.Println("Maps in GO")
+	//To make a map
+	ages := make(map[string]int)
+
+	//Adding stuff to a made up map
+	ages["John"] = 37
+	ages["Mary"] = 24
+	ages["Mary"] = 21 // Overwrites 24
+
+	fmt.Println(ages)
+
+	//Making map with predefined values
+	age := map[string]int{
+		"John": 37,
+		"Mary": 21,
+	}
+
+	fmt.Println(age)
 }
