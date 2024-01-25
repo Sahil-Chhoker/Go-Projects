99d80342c8a34110cf5aa7792eda7a23e685946f
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-25 07:50:17 -0500

    Reassigning value of Pointers

--- Files/AdvancedFunctions/pointers.go
+++ Files/AdvancedFunctions/pointers.go
@@ -14,4 +14,7 @@ func main() {
 	fmt.Println("Actual Pointer : ", myStringPtr)
 
 	fmt.Println(*myStringPtr) // Syntax to get the value stored at the address of the pointer
+	*myStringPtr = "World"    // Reassigning the value of the pointer
+
+	fmt.Println(*myStringPtr)
 }
