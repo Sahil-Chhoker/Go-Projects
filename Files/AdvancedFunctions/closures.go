71720675a239f67cf66f7e81d157aa877fac0c04
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-25 07:19:37 -0500

    .

--- Files/AdvancedFunctions/closures.go
+++ Files/AdvancedFunctions/closures.go
@@ -5,7 +5,8 @@ import (
 )
 
 func concatter() func(string) string {
-	doc := ""
+	doc := "" // The doc is referenced outside the return func,
+	// therefore it collects the value and stores it
 	return func(word string) string {
 		doc += word + " "
 		return doc
