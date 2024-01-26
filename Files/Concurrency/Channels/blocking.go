b64b341ab9217f34e3f51b788529016fe855af1a
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-26 12:03:06 -0500

    Intentional Blocking

---
+++ Files/Concurrency/Channels/blocking.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// We can block and wait until something is sent on a channel using the
+	// following syntax:
+	//<-ch(where ch is the channel name)
+	// This will block until it pops a single item off the channel, then continue,
+	// discarding the item.
+
+}
