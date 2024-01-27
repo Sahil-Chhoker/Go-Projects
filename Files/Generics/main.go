78a9bbc14a4e4755d9b0e5c739217821f766d687
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-27 01:36:27 -0500

    GEnerics

---
+++ Files/Generics/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstInts, secondlnts := splitAnySlice([]int{0, 1, 2, 3})
+	fmt.Println(firstInts, secondlnts)
+}
+
+// Without Generics we will have to make separate functions for every
+// data type
+func splitIntStice(s []int) ([]int, []int) {
+	mid := len(s) / 2
+	return s[:mid], s[mid:]
+}
+
+func splitStringStice(s []string) ([]string, []string) {
+	mid := len(s) / 2
+	return s[:mid], s[mid:]
+}
+
+//With Generics:
+
+func splitAnySlice[T any](s []T) ([]T, []T) {
+	mid := len(s) / 2
+	return s[:mid], s[mid:]
+}
