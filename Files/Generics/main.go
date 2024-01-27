29e08835dbcef925d0909bfef63442ac803d206d
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-27 02:05:46 -0500

    Constraints and new type of interfaces

--- Files/Generics/main.go
+++ Files/Generics/main.go
@@ -28,3 +28,24 @@ func splitAnySlice[T any](s []T) ([]T, []T) {
 	mid := len(s) / 2
 	return s[:mid], s[mid:]
 }
+
+// Constraints:
+type stringer interface {
+	String() string
+}
+
+func Concat[T stringer](vals []T) string {
+	result := ""
+	for _, val := range vals {
+		result += val.String()
+	}
+	return result
+}
+
+// Interface type lists:
+// Ordered is a type constraint that matches any ordered type.
+// An ordered type is one that supports the <, <=, >, and >= operators.
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32
+	//and so on...
+}
